0eba20e732d620bb59d6d08fb06364f8ffe2445a
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-11-16 12:19:28 +0500

    done

--- pages/page_eight/page.jsx
+++ pages/page_eight/page.jsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import Cover from '@/public/assets/LastpageCover.png'
+import Cover from '@/public/assets/LastpageCover.jpg'
 import Car from '@/public/assets/carvertical.png'
 import AVAV from "@/public/assets/AVAV.svg";
 const PageEight = () => {
   return (
     <div className=" h-full lg:h-[165%]" >
-        <div className='bg-black absolute h-[80vh] -z-10 screen w-full object-cover'/>
-    <Image className=" h-[80vh] p8  max-lg:absolute object-cover  opacity-90" src={Cover} width={3841} height={2159}/>
+    <Image className=" h-[80vh] p8  max-lg:absolute object-cover " src={Cover} width={3841} height={2159}/>
     <div className="  relative flex  justify-center items-center tracking-widest">
     <div className="text-left lg:-translate-y-1/2 translate-y-1/2 absolute top-[50%] hover:scale-110 transition-all duration-500 max-lg:justify-center lg:left-8 max-lg:p-16"><Image className='' src={Car} width={500} height={800}/></div>
     <div className="text-right  lg:-translate-y-1/2  absolute top-[50%]  right-8  lg:opacity-20">
