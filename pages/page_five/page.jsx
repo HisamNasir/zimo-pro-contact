2f155c5f28cd3fd7f98a5e2fd018c5df58964554
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-11-16 17:35:53 +0500

    done

--- pages/page_five/page.jsx
+++ pages/page_five/page.jsx
@@ -3,9 +3,8 @@ import Wallpaper from "@/public/assets/fifthWallpaper.png";
 import Image from "next/image";
 
 import AVAV from "@/public/assets/AVAV.svg";
-import BackArrow from '@/public/assets/BackArrow.svg'
+import BackArrow from "@/public/assets/BackArrow.svg";
 const PageFive = () => {
-  
   return (
     <div className="h-screen bg-black ">
       <Image
@@ -19,9 +18,10 @@ const PageFive = () => {
           <div>
             <h2 className=" text-4xl mb-8">Personalised for you</h2>
             <p className=" text-xs lg:text-xl lg:leading-10 opacity-50">
-            Delivering thousands of personalised alerts <br/>
-everyday, our USERS can be first in line when<br/>
-that opportunity of a lifetime is here.
+              Delivering thousands of personalised alerts <br />
+              everyday, our USERS can be first in line when
+              <br />
+              that opportunity of a lifetime is here.
             </p>
           </div>
         </div>
@@ -31,16 +31,15 @@ that opportunity of a lifetime is here.
         <div className="text-left absolute left-16">
           <div>
             <div className=" opacity-10 animate-pulse  delay-500">
-          <Image className=" h-[134x] w-[547px]" src={AVAV}/>
+              <Image className=" h-[134x] w-[547px]" src={AVAV} />
             </div>
           </div>
         </div>
 
         <button className="text-right z-10 absolute right-16 flex gap-2 items-center p-4 hover:translate-x-8 transition-all duration-500 ">
           <p>create user id</p>
-          <Image className=" h-[16px] rotate-180" src={BackArrow}/>
+          <Image className=" h-[16px] rotate-180" src={BackArrow} />
         </button>
-
       </div>
     </div>
   );
--- pages/page_three/page.jsx
+++ pages/page_three/page.jsx
@@ -1,88 +1,98 @@
-import DownArrow from '@/public/assets/DownArrow.svg'
-import React from 'react'
-import Image1 from '@/public/assets/diamond.png';
-import Image2 from '@/public/assets/island.png';
-import Image3 from '@/public/assets/realestate.png';
-import Image4 from '@/public/assets/cars.png';
-import Image5 from '@/public/assets/yachts.png';
-import Image6 from '@/public/assets/watches.png';
-import Image7 from '@/public/assets/jets.png';
-import Image8 from '@/public/assets/travelworld.png';
-import Card from '@/components/Card';
-import Image from 'next/image';
-import { useRef, useEffect } from 'react';
+import DownArrow from "@/public/assets/DownArrow.svg";
+import React from "react";
+import Image1 from "@/public/assets/diamond.png";
+import Image2 from "@/public/assets/island.png";
+import Image3 from "@/public/assets/realestate.png";
+import Image4 from "@/public/assets/cars.png";
+import Image5 from "@/public/assets/yachts.png";
+import Image6 from "@/public/assets/watches.png";
+import Image7 from "@/public/assets/jets.png";
+import Image8 from "@/public/assets/travelworld.png";
+import Card from "@/components/Card";
+import Image from "next/image";
+import { useRef, useEffect } from "react";
 
 const PageThree = () => {
   useEffect(() => {
-    const cardContainer = document.getElementById('card-container');
+    const cardContainer = document.getElementById("card-container");
 
     const handleScroll = (direction) => {
       const cardWidth = cardContainer.firstChild.offsetWidth;
 
-      if (direction === 'left') {
-        cardContainer.style.transition = 'transform 1s ease-in-out';
-        cardContainer.style.transform = 'translateX(50%)';
-      } else if (direction === 'right') {
-        cardContainer.style.transition = 'transform 1s ease-in-out';
-        cardContainer.style.transform = 'translateX(-25%)';
+      if (direction === "left") {
+        cardContainer.style.transition = "transform 1s ease-in-out";
+        cardContainer.style.transform = "translateX(50%)";
+      } else if (direction === "right") {
+        cardContainer.style.transition = "transform 1s ease-in-out";
+        cardContainer.style.transform = "translateX(-25%)";
       }
 
       setTimeout(() => {
         const firstCard = cardContainer.firstChild;
         cardContainer.appendChild(firstCard.cloneNode(true));
         cardContainer.removeChild(firstCard);
-        cardContainer.style.transition = 'none';
-        cardContainer.style.transform = 'translateX(0)';
+        cardContainer.style.transition = "none";
+        cardContainer.style.transform = "translateX(0)";
       }, 2000);
     };
 
     const animationInterval = setInterval(() => {
-      handleScroll('right');
+      handleScroll("right");
     }, 5000);
 
-    document.getElementById('left').addEventListener('click', () => handleScroll('left'));
-    document.getElementById('right').addEventListener('click', () => handleScroll('right'));
+    document
+      .getElementById("left")
+      .addEventListener("click", () => handleScroll("left"));
+    document
+      .getElementById("right")
+      .addEventListener("click", () => handleScroll("right"));
 
     return () => {
       clearInterval(animationInterval);
-      document.getElementById('left').removeEventListener('click', () => handleScroll('left'));
-      document.getElementById('right').removeEventListener('click', () => handleScroll('right'));
+      document
+        .getElementById("left")
+        .removeEventListener("click", () => handleScroll("left"));
+      document
+        .getElementById("right")
+        .removeEventListener("click", () => handleScroll("right"));
     };
   }, []);
 
   return (
-    <div className="h-screen" >
-    {/* <Image alt="" className="absolute h-full w-auto object-cover -z-160 opacity-90" src={Wallpaper} width={3841} height={2159}/> */}
-    <div className="h-full relative flex flex-col justify-between  tracking-widest">
-      <div className='flex relative'>
-    <div className=" p-8  text-xl  lg:text-[40px]">our categories</div>
+    <div className="h-screen">
+      {/* <Image alt="" className="absolute h-full w-auto object-cover -z-160 opacity-90" src={Wallpaper} width={3841} height={2159}/> */}
+      <div className="h-full relative flex flex-col justify-between  tracking-widest">
+        <div className="flex relative">
+          <div className=" p-8  text-xl  lg:text-[40px]">our categories</div>
 
-    <div className=" p-8 absolute right-0  w-[129px] flex justify-between">
-        <button id="left" className="invert rotate-90" >
-          <Image src={DownArrow} className="h-[20px]" />
-        </button>
-        <button id="right" className="invert -rotate-90">
-          <Image src={DownArrow} className="h-[20px]" />
-        </button>
-      </div>
-
-      </div>
-    <div  className="overflow-hidden  bottom-[35%] w-full" >
-        <div id="card-container" className="grid grid-flow-col space-x-2 p-2">
-          <Card image={Image1} text="diamonds" />
-          <Card image={Image2} text="private island" />
-          <Card image={Image3} text="real estate" />
-          <Card image={Image4} text="cars" />
-          <Card image={Image5} text="yachts" />
-          <Card image={Image6} text="watches" />
-          <Card image={Image7} text="jets" />
-          <Card image={Image8} text="travel the world" />
+          <div className=" p-8 absolute right-0  w-[129px] flex justify-between">
+            <button id="left" className="invert rotate-90">
+              <Image src={DownArrow} className="h-[20px]" />
+            </button>
+            <button id="right" className="invert -rotate-90">
+              <Image src={DownArrow} className="h-[20px]" />
+            </button>
+          </div>
+        </div>
+        <div className="overflow-hidden  bottom-[35%] w-full">
+          <div id="card-container" className="grid grid-flow-col space-x-2 p-2">
+            <Card image={Image1} text="diamonds" />
+            <Card image={Image2} text="private island" />
+            <Card image={Image3} text="real estate" />
+            <Card image={Image4} text="cars" />
+            <Card image={Image5} text="yachts" />
+            <Card image={Image6} text="watches" />
+            <Card image={Image7} text="jets" />
+            <Card image={Image8} text="travel the world" />
+          </div>
+        </div>
+        <div className="text-center  bottom-40 text-xl lg:text-4xl space-y-12 ">
+          <h1>one platform for all premium listings</h1>
+          <p className=" font-light">unlimited potential</p>
         </div>
       </div>
-    <div className="text-center  bottom-40 text-xl lg:text-4xl space-y-12 "><h1>one platform for all premium listings</h1><p className=' font-light'>unlimited potential</p></div>
-  </div>
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default PageThree
+export default PageThree;
