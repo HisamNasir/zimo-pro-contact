168a09f0a27bf14dea3b11e21c7188abfdd50b5e
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-11-16 17:36:02 +0500

    Update Clock.jsx

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -4,23 +4,19 @@ const Clock = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
-    // Update the current time every second
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
 
-    // Cleanup the interval on component unmount
     return () => clearInterval(intervalId);
   }, []);
 
-  // Function to format time in HH:mm format
   const formatTime = (date) => {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     return `${hours}:${minutes}`;
   };
 
-  // Function to format date with complete day and month names
   const formatDate = (date) => {
     const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -35,12 +31,10 @@ const Clock = () => {
 
   return (
     <div className=" text-right">
-      {/* Display current time in HH:mm format */}
       <div className="flex gap-3">
         <div>{formatTime(currentTime)}</div> <p>london united kingdom</p>
       </div>
 
-      {/* Display current date with complete day and month names */}
       <div className="text-[#BE9F56]">
         {formatDate(currentTime)}
       </div>
