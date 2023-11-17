635db17991c607a5f4bf749172cb0bba686a86e1
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-11-17 11:44:08 +0500

    done

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 const Clock = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [location, setLocation] = useState({ city: '', country: '' });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
 
+    // Extract city and country from timezone
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const [city, country] = extractLocation(timeZone);
+    setLocation({ city, country });
+
     return () => clearInterval(intervalId);
   }, []);
+
+  const extractLocation = (timeZone) => {
+    const [city, country] = timeZone.split('/').slice(-2);
+    return [city || timeZone, country || ''];
+  };
 
   const formatTime = (date) => {
     const hours = date.getHours().toString().padStart(2, '0');
@@ -32,7 +43,9 @@ const Clock = () => {
   return (
     <div className=" text-right">
       <div className="flex gap-3">
-        <div>{formatTime(currentTime)}</div> <p>london united kingdom</p>
+        <div>{formatTime(currentTime)}</div>
+        <p>{location.city}</p>
+        <p>{location.country}</p>
       </div>
 
       <div className="text-[#BE9F56]">
